84f19c951095d2499b3a3b087e01fbf769d70f04
Yash Chaudhary <chaudharyyash088@gmail.com>
2020-10-24 21:02:22 +0530

    added css and app.py

--- TITANIC KAGGLE/app.py
+++ TITANIC KAGGLE/app.py
@@ -18,8 +18,9 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    
-    return render_template('index.html',prediction_text="test text")
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
